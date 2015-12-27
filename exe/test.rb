223716ca7270d7e6acd54e48329a273a1b75c7c0
Minwoo Lee <ermaker@gmail.com>
2015-12-27 22:48:22 +0900

    Split files

--- exe/test.rb
+++ exe/test.rb
@@ -2,90 +2,9 @@ require 'dotenv'
 Dotenv.load
 
 require 'xingAPI'
+require 'xingAPI/xingAPI'
 require 'xingAPI/win32'
-require 'ffi'
-
-module XingAPI
-  extend FFI::Library
-  ffi_lib 'xingAPI'
-  ffi_convention :stdcall
-
-  attach_function :ETK_Connect, [:ulong, :string, :int, :int, :int, :int ], :bool
-  attach_function :ETK_IsConnected, [], :bool
-  attach_function :ETK_Disconnect, [], :bool
-
-  attach_function :ETK_Login, [:ulong, :string, :string, :string, :int, :bool], :bool
-  attach_function :ETK_Logout, [:ulong], :bool
-
-  attach_function :ETK_GetAccountListCount, [], :int
-
-  attach_function :ETK_GetAccountList, [:int, :pointer, :int], :bool
-
-  attach_function :ETK_GetClientIP, [:pointer], :void
-  attach_function :ETK_GetServerName, [:pointer], :void
-
-  attach_function :ETK_GetLastError, [], :int
-  attach_function :ETK_GetErrorMessage, [], :int
-end
-
-class Skype
-  module Communication
-    # Utilises the Windows API to send and receive Window Messages to/from
-    # Skype.
-    #
-    # This protocol is only available on Windows and Cygwin.
-    class Windows
-      attr_accessor :window
-
-      # Sets up access to Skype
-      #
-      # @see http://msdn.microsoft.com/en-us/library/bb384843.aspx Creating
-      #     Win32-Based Applications
-      def initialize(&blk)
-        instance = Win32::GetModuleHandle(nil)
-
-        @window_class = Win32::WNDCLASSEX.new
-        @window_class[:style]         = Win32::CS_HREDRAW | Win32::CS_VREDRAW
-        @window_class[:lpfnWndProc]   = blk
-        @window_class[:hInstance]     = instance
-        @window_class[:hbrBackground] = Win32::COLOR_WINDOW
-        @window_class[:lpszClassName] =
-            FFI::MemoryPointer.from_string 'ruby-skype'
-
-        @window = Win32::CreateWindowEx(Win32::WS_EX_LEFT,
-                                        FFI::Pointer.new(@window_class.handle),
-                                        'ruby-skype',
-                                        Win32::WS_OVERLAPPEDWINDOW,
-                                        0, 0, 0, 0, Win32::NULL, Win32::NULL,
-                                        instance, nil)
-      end
-
-      # Update processing.
-      #
-      # This executes a Windows event loop while there are messages still
-      # pending, then dumps back out to let other things take over and do their
-      # thing.
-      #
-      # @return [void]
-      def tick
-        msg = Win32::MSG.new
-        while Win32::PeekMessage(msg, Win32::NULL, 0, 0, Win32::PM_REMOVE) > 0
-          # puts "WAIT: #{msg[:hwnd]}, #{msg[:message]}, #{msg[:wParam]}, #{msg[:lParam]}"
-          Win32::TranslateMessage(msg)
-          Win32::DispatchMessage(msg)
-        end
-      end
-
-      def pump_up(time: 1)
-        step = 0.01
-        (time / step).to_i.times do
-          tick
-          sleep step
-        end
-      end
-    end
-  end
-end
+require 'xingAPI/windows'
 
 p ['connected: ', XingAPI.ETK_IsConnected]
 
---
+++ lib/xingAPI/xingAPI.rb
@@ -0,0 +1,24 @@
+require 'ffi'
+
+module XingAPI
+  extend FFI::Library
+  ffi_lib 'xingAPI'
+  ffi_convention :stdcall
+
+  attach_function :ETK_Connect, [:ulong, :string, :int, :int, :int, :int ], :bool
+  attach_function :ETK_IsConnected, [], :bool
+  attach_function :ETK_Disconnect, [], :bool
+
+  attach_function :ETK_Login, [:ulong, :string, :string, :string, :int, :bool], :bool
+  attach_function :ETK_Logout, [:ulong], :bool
+
+  attach_function :ETK_GetAccountListCount, [], :int
+
+  attach_function :ETK_GetAccountList, [:int, :pointer, :int], :bool
+
+  attach_function :ETK_GetClientIP, [:pointer], :void
+  attach_function :ETK_GetServerName, [:pointer], :void
+
+  attach_function :ETK_GetLastError, [], :int
+  attach_function :ETK_GetErrorMessage, [], :int
+end
