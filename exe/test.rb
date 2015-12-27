1d27b6994d76de935443db5bd365fe70fe7db507
Minwoo Lee <ermaker@gmail.com>
2015-12-27 22:52:15 +0900

    Rename

--- exe/test.rb
+++ exe/test.rb
@@ -6,53 +6,53 @@ require 'xingAPI/xingAPI'
 require 'xingAPI/win32'
 require 'xingAPI/windows'
 
-p ['connected: ', XingAPI.ETK_IsConnected]
+p ['connected: ', XingAPI::XingAPI.ETK_IsConnected]
 
 hwnd = 0
-win = Skype::Communication::Windows.new do |hwnd, msgid, wparam, lparam|
+win = XingAPI::Windows.new do |hwnd, msgid, wparam, lparam|
   puts "WM: #{hwnd}, #{msgid}, #{wparam}, #{lparam}"
   if msgid == 1029
     puts "wparam: #{FFI::Pointer.new(:string, wparam).read_string.force_encoding('cp949')}"
     puts "lparam: #{FFI::Pointer.new(:string, lparam).read_string.force_encoding('cp949')}"
   end
-  Skype::Communication::Windows::Win32::DefWindowProc(hwnd, msgid, wparam, lparam)
+  XingAPI::Windows::Win32::DefWindowProc(hwnd, msgid, wparam, lparam)
 end
 p ['win', win]
 hwnd = win.window
 
 p ['hwnd', hwnd]
 
-result = XingAPI.ETK_Connect(hwnd, "hts.etrade.co.kr", 20001, 1024, -1, 512)
-# result = XingAPI.ETK_Connect(hwnd, "demo.etrade.co.kr", 20001, 1024, -1, 512)
+result = XingAPI::XingAPI.ETK_Connect(hwnd, "hts.etrade.co.kr", 20001, 1024, -1, 512)
+# result = XingAPI::XingAPI.ETK_Connect(hwnd, "demo.etrade.co.kr", 20001, 1024, -1, 512)
 p ['connect: ', result]
 
-result = XingAPI.ETK_Login(hwnd, ENV['ID'], ENV['PASS'], ENV['PASS2'], 0, false)
+result = XingAPI::XingAPI.ETK_Login(hwnd, ENV['ID'], ENV['PASS'], ENV['PASS2'], 0, false)
 p ['try_login', result]
 
 win.pump_up
 
-count = XingAPI.ETK_GetAccountListCount()
+count = XingAPI::XingAPI.ETK_GetAccountListCount()
 p ['account list count', count]
 
 count.times do |idx|
   out = FFI::MemoryPointer.new(256)
-  XingAPI.ETK_GetAccountList(idx, out, out.size)
+  XingAPI::XingAPI.ETK_GetAccountList(idx, out, out.size)
   p out.read_string
 end
 
 out = FFI::MemoryPointer.new(256)
-XingAPI.ETK_GetClientIP(out)
+XingAPI::XingAPI.ETK_GetClientIP(out)
 p out.read_string
 
 out = FFI::MemoryPointer.new(256)
-XingAPI.ETK_GetServerName(out)
+XingAPI::XingAPI.ETK_GetServerName(out)
 p out.read_string
 
-result = XingAPI.ETK_Logout(hwnd)
+result = XingAPI::XingAPI.ETK_Logout(hwnd)
 p ['try_logout', result]
 
 win.pump_up
 
-p ['Disconnect: ', XingAPI.ETK_Disconnect]
+p ['Disconnect: ', XingAPI::XingAPI.ETK_Disconnect]
 
 win.pump_up
--- lib/xingAPI/windows.rb
+++ lib/xingAPI/windows.rb
@@ -1,59 +1,58 @@
+require 'xingAPI/win32'
 require 'ffi'
 
-class Skype
-  module Communication
-    # Utilises the Windows API to send and receive Window Messages to/from
-    # Skype.
+module XingAPI
+  # Utilises the Windows API to send and receive Window Messages to/from
+  # Skype.
+  #
+  # This protocol is only available on Windows and Cygwin.
+  class Windows
+    attr_accessor :window
+
+    # Sets up access to Skype
     #
-    # This protocol is only available on Windows and Cygwin.
-    class Windows
-      attr_accessor :window
+    # @see http://msdn.microsoft.com/en-us/library/bb384843.aspx Creating
+    #     Win32-Based Applications
+    def initialize(&blk)
+      instance = Win32::GetModuleHandle(nil)
 
-      # Sets up access to Skype
-      #
-      # @see http://msdn.microsoft.com/en-us/library/bb384843.aspx Creating
-      #     Win32-Based Applications
-      def initialize(&blk)
-        instance = Win32::GetModuleHandle(nil)
+      @window_class = Win32::WNDCLASSEX.new
+      @window_class[:style]         = Win32::CS_HREDRAW | Win32::CS_VREDRAW
+      @window_class[:lpfnWndProc]   = blk
+      @window_class[:hInstance]     = instance
+      @window_class[:hbrBackground] = Win32::COLOR_WINDOW
+      @window_class[:lpszClassName] =
+          FFI::MemoryPointer.from_string 'xingAPI-ruby'
 
-        @window_class = Win32::WNDCLASSEX.new
-        @window_class[:style]         = Win32::CS_HREDRAW | Win32::CS_VREDRAW
-        @window_class[:lpfnWndProc]   = blk
-        @window_class[:hInstance]     = instance
-        @window_class[:hbrBackground] = Win32::COLOR_WINDOW
-        @window_class[:lpszClassName] =
-            FFI::MemoryPointer.from_string 'ruby-skype'
+      @window = Win32::CreateWindowEx(Win32::WS_EX_LEFT,
+                                      FFI::Pointer.new(@window_class.handle),
+                                      'xingAPI-ruby',
+                                      Win32::WS_OVERLAPPEDWINDOW,
+                                      0, 0, 0, 0, Win32::NULL, Win32::NULL,
+                                      instance, nil)
+    end
 
-        @window = Win32::CreateWindowEx(Win32::WS_EX_LEFT,
-                                        FFI::Pointer.new(@window_class.handle),
-                                        'ruby-skype',
-                                        Win32::WS_OVERLAPPEDWINDOW,
-                                        0, 0, 0, 0, Win32::NULL, Win32::NULL,
-                                        instance, nil)
+    # Update processing.
+    #
+    # This executes a Windows event loop while there are messages still
+    # pending, then dumps back out to let other things take over and do their
+    # thing.
+    #
+    # @return [void]
+    def tick
+      msg = Win32::MSG.new
+      while Win32::PeekMessage(msg, Win32::NULL, 0, 0, Win32::PM_REMOVE) > 0
+        # puts "WAIT: #{msg[:hwnd]}, #{msg[:message]}, #{msg[:wParam]}, #{msg[:lParam]}"
+        Win32::TranslateMessage(msg)
+        Win32::DispatchMessage(msg)
       end
+    end
 
-      # Update processing.
-      #
-      # This executes a Windows event loop while there are messages still
-      # pending, then dumps back out to let other things take over and do their
-      # thing.
-      #
-      # @return [void]
-      def tick
-        msg = Win32::MSG.new
-        while Win32::PeekMessage(msg, Win32::NULL, 0, 0, Win32::PM_REMOVE) > 0
-          # puts "WAIT: #{msg[:hwnd]}, #{msg[:message]}, #{msg[:wParam]}, #{msg[:lParam]}"
-          Win32::TranslateMessage(msg)
-          Win32::DispatchMessage(msg)
-        end
-      end
-
-      def pump_up(time: 1)
-        step = 0.01
-        (time / step).to_i.times do
-          tick
-          sleep step
-        end
+    def pump_up(time: 1)
+      step = 0.01
+      (time / step).to_i.times do
+        tick
+        sleep step
       end
     end
   end
