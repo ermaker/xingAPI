c07861fac049d03c5e16d46823d009d304ec443a
Minwoo Lee <ermaker@gmail.com>
2016-01-04 17:36:26 +0900

    Modify connector

--- exe/test.rb
+++ exe/test.rb
@@ -10,6 +10,7 @@ require 'multi_json'
 module XingAPI
   class Connector
     RUN_COMMAND = %w[bundle exec ruby -Ilib exe\\run.rb]
+    # RUN_COMMAND = %w[bundle exec run]
     def initialize
       @stdin, @stdout, @wait_thr = Open3.popen2(*RUN_COMMAND)
     end
@@ -17,9 +18,14 @@ module XingAPI
     def run_(*args)
       @stdin.puts(MultiJson.dump(args))
     end
+
     def run(*args)
       run_(*args)
       MultiJson.load(@stdout.gets)
+    end
+
+    def method_missing(*args)
+      run(*args)
     end
 
     def finish
@@ -31,74 +37,25 @@ module XingAPI
   end
 end
 
-c = XingAPI::Connector.new
-p c.run(:account, 0)
-response = c.run(:tr_t1901, '122630')
-puts "#{response['data']['hname'].strip}: #{response['data']['price']} (#{response['data']['diff']}%)"
-puts response['message']
-c.finish
-
-exit
-
-require 'xingAPI/win32'
-require 'message_window'
-
-mw = MessageWindow.new
-
-result = mw.test_
-p format('return result: %04x', result)
-
-sleep 1
-
-hwnd = $_message_window_hwnd
-
-loop do
-  XingAPI::Windows::Win32.PostMessage(hwnd, 0x400, 0, 0)
-  p format('%04x', mw.queue.pop) unless mw.queue.empty?
+module XingAPI
+  @logger = Logger.new(STDOUT)
 end
 
-exit
-
-mw = MessageWindow.new
-result = mw.create
-hwnd = result
-
-p format('%04x', hwnd)
-
-# hwnd = -1
-# hwnd = 0
-
-XingAPI::Windows::Win32.PostMessage(hwnd, 0x400, 0, 0)
-
-thread_id_handle = mw.pump
-
-loop do
-  XingAPI::Windows::Win32.PostMessage(hwnd, 0x400, 0, 0)
-  p format('%04x', mw.queue.pop) unless mw.queue.empty?
-  sleep 0.01
+api = XingAPI::Connector.new
+if true
+  ::XingAPI::logger.info { "account: #{api.account(0)}" }
 end
-
-exit
-
-# api = XingAPI::API.new(ENV['IP'], ENV['PORT'], ENV['ID'], ENV['PASS'], ENV['PASS2'])
-# if true
-XingAPI::API.new(ENV['IP'], ENV['PORT'], ENV['ID'], ENV['PASS'], ENV['PASS2']) do |api|
-  if true
-    ::XingAPI::logger.info { "account: #{api.account(0)}" }
-  end
-
-  if false
-    result = api.tr_CSPAT00600(ENV['ACCOUNT'], ENV['ACCOUNT_PASS'], '114800', 1, :sell)
-    ::XingAPI::logger.info { result[:message] }
-  end
-
-  if false
-    3.times do
-      result = api.tr_t1901('122630')
-      ::XingAPI::logger.info do
-        "#{result[:data][:hname].strip}: #{result[:data][:price]} (#{result[:data][:diff]}%)"
-      end
-      sleep 2
+if false
+  result = api.tr_CSPAT00600(ENV['ACCOUNT'], ENV['ACCOUNT_PASS'], '114800', 1, :sell)
+  ::XingAPI::logger.info { result[:message] }
+end
+if true
+  3.times do
+    result = api.tr_t1901('122630')
+    ::XingAPI::logger.info do
+      "#{result['data']['hname'].strip}: #{result['data']['price']} (#{result['data']['diff']}%)"
     end
+    sleep 2
   end
 end
+api.finish
